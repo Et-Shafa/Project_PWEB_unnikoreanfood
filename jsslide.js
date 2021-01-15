1bd14b3905ae94d2d870ce49f7c5f848199b570c
Et-shafa <naadiramalaika.atsiila@gmail.com>
2021-01-15 10:52:49 +0700

    new

--- jsslide.js
+++ jsslide.js
@@ -50,3 +50,12 @@ function inputan(){
     }
     
 }
+
+
+function sidebar_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+  
+function sidebar_close() {
+document.getElementById("mySidebar").style.display = "none";
+}
